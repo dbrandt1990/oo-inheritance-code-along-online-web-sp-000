6168bc91738002813f3093693a78afe7307904d6
Danny Brandt <dvbrandt90@gmail.com>
2021-01-11 22:42:09 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,11 @@
 require_relative "./vehicle.rb"
 class Car < Vehicle
   
+<<<<<<< HEAD
    def go 
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
    end 
 
+=======
+>>>>>>> 42d9313e7d14809f52796c55f8328e1658adffb9
 end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,5 +1,9 @@
 class Vehicle
+<<<<<<< HEAD
   attr_accessor :wheel_size, :wheel_number
+=======
+  attr_reader :wheel_size, :wheel_number
+>>>>>>> 42d9313e7d14809f52796c55f8328e1658adffb9
 
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
